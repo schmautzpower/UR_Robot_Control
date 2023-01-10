a93e2412de563d5c410e22898b60019216c3560b
Benjamin Byrdeck <Benjamin.Byrdeck@orthocaps.de>
2023-01-10 14:41:52 +0100

    Add Dashboard Server

--- robot_control.py
+++ robot_control.py
@@ -420,6 +420,419 @@ class RTDE:
         return False
 
 
+class DashboardServer:
+    def __init__(self, robot_ip: str):
+        self.ds = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.ds.settimeout(1)
+        self.robot_ip = robot_ip
+        self.port = 29999
+        self.ds.connect((self.robot_ip, self.port))
+        logging.info(self.ds.recv(1028).decode())
+
+    def reconnect(self):
+        try:
+            self.ds.connect((self.robot_ip, self.port))
+        except TimeoutError as e:
+            logging.error(f'Connection failed {str(e)}')
+
+    def disconnect(self):
+        self.ds.close()
+
+    def __send(self, command: str):
+        command += '\n'
+        self.ds.sendall(command.encode())
+        try:
+            re = self.ds.recv(4096).decode()
+        except Exception as e:
+            re = f'No Response {e}'
+        logging.info(re)
+        return re
+
+    def load(self, program: str):
+        """Returns when both program and associated installation has loaded (or failed). 
+        The load command fails if the associated installation requires confirmation of safety. 
+        The return value in this case will be 'Error while loading program'.
+
+        Args:
+            program (str): program name or path
+
+        Returns:
+            _type_: str
+            On success:
+            • "Loadingprogram: <program.urp>"
+            On Failure:
+            • "Filenotfound: <program.urp>"
+            • "Errorwhileloading program:
+            <program.urp>"
+        """
+        return self.__send(f'load {program}')
+
+    def play(self):
+        """Returns failure if the program fails to start. In previous versions this did not happen in all cases.
+
+        Returns:
+            _type_: str
+            On success:
+            • "Startingprogram"
+            On failure:
+            • "Failedtoexecute: play"
+        """
+        return self.__send('play')
+
+    def stop(self):
+        """Returns failure if the program fails to stop. In previous versions this did not happen in all cases..
+
+        Returns:
+            _type_: str
+            On success:
+            • "Stopped"
+            On failure:
+            • "Failedtoexecute: stop"
+        """
+        return self.__send('stop')
+
+    def pause(self):
+        """Returns failure if the program fails to pause. In previous versions this did not happen in all cases.
+
+        Returns:
+            _type_: str
+            On success:
+            • "Pausingprogram"
+            On failure:
+            • "Failedtoexecute: pause"
+        """
+        return self.__send('pause')
+
+    def quit(self):
+        """Closes connection
+
+        Returns:
+            _type_: str
+            "Disconnected"
+        """
+        return self.__send('quit')
+
+    def shutdown(self):
+        """Shuts down and turns off robot and controller
+
+        Returns:
+            _type_: str
+            "Shutting down"
+        """
+        return self.__send('shutdown')
+
+    def running(self):
+        """Execution state enquiry
+
+        Returns:
+            _type_: str
+            "Program running: true" OR "Program running: false"
+        """
+        return self.__send('running')
+
+    def robotmode(self):
+        """Robot mode enquiry
+
+        Returns:
+            _type_: str
+            text is returned "Robotmode: <mode>", where <mode> is
+            • NO_CONTROLLER • DISCONNECTED
+            • CONFIRM_SAFETY • BOOTING
+            • POWER_OFF • POWER_ON • IDLE
+            • BACKDRIVE • RUNNING
+        """
+        return self.__send('robotmode')
+
+    def get_loaded_program(self):
+        """Which program is loaded
+
+        Returns:
+            _type_: str 
+            "Loaded program: <path to loaded program file>" OR "No program loaded"
+        """
+        return self.__send('get loaded program')
+
+    def popup(self, msg: str):
+        """The popup-text will be translated to the selected language, if the text exists in the language file
+
+        Args:
+            msg (str): popup text
+
+        Returns:
+            _type_: str
+            "showing popup"
+        """
+        return self.__send(f'popup {msg}')
+
+    def close_popup(self):
+        """Closes the popup
+
+        Returns:
+            _type_: str
+            "closing popup"
+        """
+        return self.__send('close popup')
+
+    def add_to_log(self, msg: str):
+        """Adds log-message to the Log history
+
+        Args:
+            msg (str): log-message
+
+        Returns:
+            _type_: str
+            "Added log message" Or "No log message to add"
+        """
+        return self.__send(f'addToLog {msg}')
+
+    def is_program_saved(self):
+        """Returns the save state of the active program and path to loaded program file.
+
+        Returns:
+            _type_: str
+            "true <program.name>" OR "false <program.name>"
+        """
+        return self.__send('isProgramSaved')
+
+    def program_state(self):
+        """Returns the state of the active program and path to loaded program file, or STOPPED if no program is loaded
+
+        Returns:
+            _type_: str
+            STOPPED" if no program is running
+            "PLAYING" if program is running
+            "PAUSED" if program is paused
+        """
+        return self.__send('programState')
+
+    def polyscope_version(self):
+        """Returns version information for the UR software installed on the robot
+
+        Returns:
+            _type_: str
+            version number, like "URSoftware 5.12.0.1101319 (Mar 22 2022)"
+        """
+        return self.__send('PolyscopeVersion')
+
+    def version(self):
+        """Returns the version number of the UR software installed on the robot
+
+        Returns:
+            _type_: str
+            Software version number, e.g. "5.13.0.11013"
+        """
+        return self.__send('version')
+
+    def set_operational_mode(self, mode: str):
+        """Controls the operational mode. See User manual for details.
+            Warning: This functionality is intended for using e.g. Ethernet based Key Card Readers 
+            to switch operational modes. 
+            The device for switching operational mode should be placed in vicinity to the robot.
+            If this function is called the operational mode cannot be changed from PolyScope, 
+            and the user password is disabled.
+
+        Args:
+            mode (str): manual, automatic
+
+        Returns:
+            _type_: str
+            "Setting operational mode: <mode>" OR "Failed setting operational mode: <mode>"
+            • manual=Loading and editing programs is allowed
+            • automatic=Loading and editing programs and installations is not allowed, only playing programs
+        """
+        return self.__send(f'set operational mode {mode}')
+
+    def get_operational_mode(self):
+        """Returns the operational mode as MANUAL or AUTOMATIC if the password has been set for Mode in Settings.
+            Returns NONE if the password has not been set.      
+
+        Returns:
+            _type_: str
+            MANUAL, AUTOMATIC or NONE
+        """
+        return self.__send('get operational mode')
+
+    def clear_operational_mode(self):
+        """If this function is called the operational mode can again be changed from PolyScope, 
+            and the user password is enabled. 
+
+        Returns:
+            _type_: str
+            "operational mode is no longer controlled by Dashboard Server"
+        """
+        return self.__send('clear operational mode')
+
+    def power_on(self):
+        """Powers on the robot arm
+
+        Returns:
+            _type_: str
+            "Powering on"
+        """
+        return self.__send('power on')
+
+    def power_off(self):
+        """Powers off the robot arm
+
+        Returns:
+            _type_: str
+            "Powering off"
+        """
+        return self.__send('power off')
+
+    def brake_release(self):
+        """Releases the brakes
+
+        Returns:
+            _type_: str
+            "Brake releasing"
+        """
+        return self.__send('brake release')
+
+    def safety_status(self):
+        """Safety status inquiry.
+            This differs from 'safetymode' by specifying if a given Safeguard Stop was caused by the permanent 
+            safeguard I/O stop, a configurable I/O automatic mode safeguard stop or a configurable I/O three 
+            position enabling device stop. 
+
+        Returns:
+            _type_: str
+            "Safetystatus: <status>", where <status> is
+            • NORMAL
+            • REDUCED
+            • PROTECTIVE_STOP
+            • RECOVERY
+            • SAFEGUARD_STOP
+            • SYSTEM_ EMERGENCY_STOP
+            • ROBOT_ EMERGENCY_STOP
+            • VIOLATION
+            • FAULT
+            • AUTOMATIC_MODE_ SAFEGUARD_STOP
+            • SYSTEM_THREE_ POSITION_ ENABLING_STOP
+        """
+        return self.__send('safetystatus')
+
+    def unlock_protective_stop(self):
+        """Closes the current popup and unlocks protective stop. 
+        The unlock protective stop command fails if less than 5 seconds has passed since the protective stop occurred.
+
+        Returns:
+            _type_: str
+            On success:
+            • "Protectivestop releasing"
+            On failure:
+            • "Cannotunlock protective stop until 5s after occurrence. Always inspect cause of protective stop before unlocking"
+        """
+        return self.__send('unlock protective stop')
+
+    def close_safety_popup(self):
+        """Closes a safety popup
+
+        Returns:
+            _type_: str
+            "closing safety popup"
+        """
+        return self.__send('close safety popup')
+
+    def load_installation(self, installation: str):
+        """Loads the specified installation file but does not return until the load has completed (or failed).
+            The load command fails if the associated installation requires confirmation of safety. 
+            The return value will be 'Failed to load installation'.
+
+        Args:
+            installation (str): default.installation
+
+        Returns:
+            _type_: str
+            On success:
+            • "Loadinginstallation: <default.installatio n>"
+            On failure:
+            • "Filenotfound: <default.installatio n>"
+            • "Failedtoload installation: <default.installatio n>"
+        """
+        return self.__send(f'load installation {installation}')
+
+    def restart_safety(self):
+        """Used when robot gets a safety fault or violation to restart the safety. 
+        After safety has been rebooted the robot will be in Power Off. 
+        IMPORTANT: You should always ensure it is okay to restart the system. 
+        It is highly recommended to check the error log before using this command (either via PolyScope 
+        or e.g. ssh connection).
+
+        Returns:
+            _type_: str
+            Restarting safety
+        """
+        return self.__send('restart safety')
+
+    def remote_control(self):
+        """Returns the remote control status of the robot.
+        If the robot is in remote control it returns true and if remote control is disabled or robot is 
+        in local control it returns false.
+
+        Returns:
+            _type_: str
+            "true" or "false"
+        """
+        return self.__send('is in remote control')
+
+    def get_serial_number(self):
+        """Returns serial number of the robot.
+
+        Returns:
+            _type_: str
+            Serial number like "20175599999"
+        """
+        return self.__send('get serial number')
+
+    def get_robot_model(self):
+        """Returns the robot model
+
+        Returns:
+            _type_: str
+            UR3, UR5, UR10, UR16
+        """
+        return self.__send('get robot model')
+
+    def generate_flight_report(self, type: str):
+        """Triggers a Flight Report of the following type:
+            • Controller- report with information specific for diagnosing controller errors. 
+            For example, in case of protective stops, faults or violations.
+            • Software- report with information specific for polyscope software failures.
+            • System- report with information about robot configuration, programs, installations etc.
+            It is required to wait at least 30 seconds between triggering software or controller reports.
+
+        Args:
+            type (str): possible report types are:
+            • controller 
+            • software 
+            • system
+
+        Returns:
+            _type_: str
+            On success report id is printed.
+            Error Message on a failure. Command can take few minutes to complete.
+        """
+        return self.__send(f'generate flight report {type}')
+
+    def generate_support_file(self, path: str):
+        """Generates a flight report of the type "System" and creates a compressed collection of 
+        all the existing flight reports on the robot along with the generated flight report.
+        Result file ur_[robot serial number]_ YYYY-MM-DD_HH- MM-SS.zip is saved inside <Directory path>
+
+        Args:
+            path (str): <Directory path> where <Directory path> represents path to an already existing 
+            directory location inside the programs directory. 
+            In particular path can point to special usbdisk subfolders inside programs folder.
+
+        Returns:
+            _type_: str
+            On success "Completed successfully: <result file name>" is printed otherwise an error message with possible cause of the error is shown.
+            Command can take up to 10 minutes to complete.
+        """
+        return self.__send(f'generate support file {path}')
+
+
 def normalize(numbers, rounds=5):
     index = 0
     for i in numbers:
@@ -441,7 +854,7 @@ def radians_to_degree(radian: float):
 
 if __name__ == '__main__':
     import time
-    robot_ip = '192.168.11.11'
+    robot_ip = '192.168.8.229'
     info = RTDE(robot_ip)
     control = URControl(robot_ip, True)
     icontrol = URControl(robot_ip, False)
